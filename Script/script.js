7a78172e51fb433a63ee30b8551cf04e20304d7a
nikkou-0814 <125991634+nikkou-0814@users.noreply.github.com>
2023-10-16 08:39:36 +0900

    fix script style

---
+++ Script/script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        var scrollingElement = document.getElementById("hellomodal");
+        scrollingElement.style.top = "-100vh";
+    }, 3000);
+});
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        var scrollingElement = document.getElementById("hello-text");
+        scrollingElement.style.top = "-100vh";
+    }, 3000);
+});
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        var scrollingElement = document.getElementById("hello-back");
+        scrollingElement.style.top = "-100vh";
+    }, 3000);
+});
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        var scrollingElement = document.getElementById("helloimg");
+        scrollingElement.style.top = "-100vh";
+    }, 3000);
+});
