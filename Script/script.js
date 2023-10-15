593b58aa4b0ff6113ef5e8d8a7da38c4903ff5e3
nikkou-0814 <125991634+nikkou-0814@users.noreply.github.com>
2023-10-15 23:21:22 +0900

    fix script

--- Script/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    setTimeout(function() {
-        var scrollingElement = document.getElementById("hellomodal");
-        scrollingElement.style.top = "-100vh";
-    }, 3000);
-});
