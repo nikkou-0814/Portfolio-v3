59e4c808434a95178aaff660a8f245335c273e1c
nikkou-0814 <125991634+nikkou-0814@users.noreply.github.com>
2023-10-16 09:04:49 +0900

    fix hellomodal

--- Script/script.js
+++ Script/script.js
@@ -1,24 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
         var scrollingElement = document.getElementById("hellomodal");
-        scrollingElement.style.top = "-100vh";
+        scrollingElement.style.top = "-120vh";
     }, 3000);
 });
 document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
         var scrollingElement = document.getElementById("hello-text");
-        scrollingElement.style.top = "-100vh";
+        scrollingElement.style.top = "-120vh";
     }, 3000);
 });
 document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
         var scrollingElement = document.getElementById("hello-back");
-        scrollingElement.style.top = "-100vh";
+        scrollingElement.style.top = "-120vh";
     }, 3000);
 });
 document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
         var scrollingElement = document.getElementById("helloimg");
-        scrollingElement.style.top = "-100vh";
+        scrollingElement.style.top = "-120vh";
     }, 3000);
 });
