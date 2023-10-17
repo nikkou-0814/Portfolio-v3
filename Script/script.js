5cb0b8f46919371e9c44f70d9593043ed35eb6b3
nikkou-0814 <125991634+nikkou-0814@users.noreply.github.com>
2023-10-18 02:14:41 +0900

    add ityped, portfoliover and fix style script

--- Script/script.js
+++ Script/script.js
@@ -2,23 +2,40 @@ document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
         var scrollingElement = document.getElementById("hellomodal");
         scrollingElement.style.top = "-120vh";
-    }, 3000);
+    }, 5400);
 });
 document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
         var scrollingElement = document.getElementById("hello-text");
         scrollingElement.style.top = "-120vh";
-    }, 3000);
+    }, 5400);
 });
 document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
         var scrollingElement = document.getElementById("hello-back");
         scrollingElement.style.top = "-120vh";
-    }, 3000);
+    }, 5400);
 });
 document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
         var scrollingElement = document.getElementById("helloimg");
         scrollingElement.style.top = "-120vh";
-    }, 3000);
+    }, 5400);
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        var element = document.getElementById("hello-mo-div");
+        element.style.opacity = 1;
+    }, 300);
+});
+
+ityped.init(document.querySelector("#ityped"), {
+    strings: ['Welcome to my Portfolio!', 'Thank you for viewing! :)']
+
+    ,typeSpeed:  50,
+    startDelay: 800,
+    backSpeed:  20,
+    loop: false,
+    showCursor: true,
+})
